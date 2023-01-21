07bfe72e0d77d7deaa1a3283533a08cb226df666
Velcro <davesimpcontrol@gmail.com>
2023-01-21 09:17:47 -0700

    added vava-html-css- and asset picture

---
+++ Assets/js/script.js
@@ -0,0 +1,6 @@
+var wordBlank = document.querySelector("");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
